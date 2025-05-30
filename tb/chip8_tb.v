218c95f82061cd98db1cb0deb9af673c8ae4804b
prxnav2005 <pranavmofficial2005@gmail.com>
2025-05-30 19:15:26 +0530

    chore: clean up testbench code

--- tb/chip8_tb.v
+++ tb/chip8_tb.v
@@ -3,13 +3,12 @@ module chip8_tb;
   reg clk = 0;
   reg reset = 1;
   reg [15:0] keys = 0;
-  wire [63:0] display_row [31:0];
+  wire [2047:0] display_row;
   
   always
     #5 clk = ~clk;
-  end
   
-  chip8_top DUT(.clk(clk), .reset(reset), .keys(keys), .display_row(display_row));
+  chip8_top DUT(.clk(clk), .reset(reset), .keys(keys), .display(display));
   
   initial
     begin

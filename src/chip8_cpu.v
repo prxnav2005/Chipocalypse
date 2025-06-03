ee541a91d844241c321e570af216a383bfb54a28
prxnav2005 <pranavmofficial2005@gmail.com>
2025-06-03 08:49:26 +0530

    feat: new opcodes implemented

--- src/chip8_cpu.v
+++ src/chip8_cpu.v
@@ -1,14 +1,23 @@
 // Code your design here
+// Code your design here
 
 // The lines in grey are debug statements that I added for my own clarity.
 
 module chip8_cpu(input wire clk, reset, input wire [7:0] mem_data_in, input wire [15:0] keys, output reg mem_read, output reg [11:0] mem_addr_out, output reg [2047:0] display, output reg [7:0] mem_data_out, output reg mem_write);
   
-  reg [11:0] pc, I;
+  reg [11:0] pc, I, pixel_index;
   reg [15:0] opcode;
   reg [2:0] state;
   reg [7:0] V [0:15];
-  reg [7:0] rand_val;
+  reg [7:0] rand_val, delay_timer, sprite_byte;
+  reg [11:0] stack [0:15];
+  reg [3:0] sp;
+  reg [20:0] clk_divider;
+  reg [3:0] draw_row;
+  reg [2047:0] new_display;
+  reg [5:0] x,y,bit_pos;
+  reg collision_flag;
+  wire clock_tick;
   integer i;
   
   localparam FETCH1 = 0;
@@ -18,6 +27,8 @@ module chip8_cpu(input wire clk, reset, input wire [7:0] mem_data_in, input wire
   localparam STORE_BCD_1 = 4;
   localparam STORE_BCD_2 = 5;
   localparam STORE_BCD_3 = 6;
+  localparam DRAW_SPITE = 7;
+  localparam FETCH_SPRITE_BYTE = 8;
   
   reg [7:0] opcode_hi, bcd_value;
   
@@ -31,6 +42,9 @@ module chip8_cpu(input wire clk, reset, input wire [7:0] mem_data_in, input wire
           I <= 12'd0;
           opcode <= 16'd0;
           mem_write <= 0;
+          sp <= 0;
+          collision_flag <= 0;
+          new_display <= 2048'd0;
           for(i = 0; i < 16; i = i+1)
             V[i] <= 8'd0;
           display <= 2048'd0;
@@ -69,8 +83,17 @@ module chip8_cpu(input wire clk, reset, input wire [7:0] mem_data_in, input wire
                   if(opcode == 16'h00E0)
                     begin
                       display <= 2048'd0;
+                      pc <= pc + 2;
                     end
-                  pc <= pc + 2;
+                  else if(opcode == 16'h00EE)
+                    begin
+                      sp <= sp - 1;
+                      pc <= stack[sp];
+                    end
+                  else
+                    begin
+                      pc <= pc + 2;
+                    end
                   state <= FETCH1;
                 end
                 
@@ -92,11 +115,12 @@ module chip8_cpu(input wire clk, reset, input wire [7:0] mem_data_in, input wire
                 end
                 
                 4'hD: begin
-                  display[0] <= ~display[0];
-                  pc <= pc + 2;
-                  state <= FETCH1;
+                  draw_row <= 0;
+                  collision_flag <= 0;
+                  new_display <= display;
+                  state <= DRAW_SPRITE;
                 end
-                
+
                 4'hA: begin
                   I <= opcode[11:0];
                   pc <= pc + 2;
@@ -125,6 +149,11 @@ module chip8_cpu(input wire clk, reset, input wire [7:0] mem_data_in, input wire
                       state <= FETCH1;
                     end
                     
+                    8'h07: begin
+                      V[opcode[11:8]] <= delay_timer;
+                      pc <= pc + 2;
+                      state <= FETCH1;
+                    end
                     
                     default: begin
                       pc <= pc + 2;
@@ -190,6 +219,55 @@ module chip8_cpu(input wire clk, reset, input wire [7:0] mem_data_in, input wire
                       state <= FETCH1;
                     end
                     
+                    4'h4: begin
+                      {V[15], V[opcode[11:8]]} = V[opcode[11:8]] + V[opcode[7:4]];
+                      pc <= pc + 2;
+                      state <= FETCH1;
+                    end
+                    
+                    4'h5: begin
+                      if (V[opcode[11:8]] >= V[opcode[7:4]])
+                        begin
+                          V[15] = 1;
+                          V[opcode[11:8]] = V[opcode[11:8]] - V[opcode[7:4]];
+                        end
+                      else
+                        begin
+                          V[15] = 0;
+                          V[opcode[11:8]] = V[opcode[11:8]] - V[opcode[7:4]];
+                        end
+                      pc <= pc + 2;
+                      state <= FETCH1;
+                    end
+                    
+                    4'h6: begin
+                      V[15] = V[opcode[11:8]][0];
+                      V[opcode[11:8]] = V[opcode[11:8]] >> 1;
+                      pc <= pc + 2;
+                      state <= FETCH1;
+                    end
+                    
+                    4'h7: begin
+                      if(V[opcode[7:4]] > V[opcode[11:8]])
+                        begin
+                          V[15] = 1;
+                        end
+                      else
+                        begin
+                          V[15] = 0;
+                        end
+                      V[opcode[11:8]] = V[opcode[7:4]] - V[opcode[11:8]];
+                      pc <= pc + 2;
+                      state <= FETCH1;
+                    end
+                    
+                    4'hE: begin
+                      V[15] = (V[opcode[11:8]] & 8'h80) >> 7;
+                      V[opcode[11:8]] = V[opcode[11:8]] << 1;
+                      pc <= pc + 2;
+                      state <= FETCH1;
+                    end
+                    
                     default: begin
                       pc <= pc + 2;
                       state <= FETCH1;
@@ -235,8 +313,24 @@ module chip8_cpu(input wire clk, reset, input wire [7:0] mem_data_in, input wire
                   endcase
                 end
                 
+                4'h9: begin
+                  if(opcode[3:0] == 4'h0)
+                    begin
+                      if(V[opcode[11:8]] != V[opcode[7:4]])
+                        pc <= pc + 4;
+                      else
+                        pc <= pc + 2;
+                    end
+                  else
+                    begin
+                      pc <= pc + 2;
+                      state <= FETCH1;
+                    end
                 
-                
+                  4'hB: begin
+                    pc <= opcode[11:0] + V[0];
+                    state <= FETCH1;
+                  end
                 
                 STORE_BCD_1: begin
                   mem_addr_out <= I + 1;
@@ -256,6 +350,45 @@ module chip8_cpu(input wire clk, reset, input wire [7:0] mem_data_in, input wire
                   pc <= pc + 2;
                   state <= FETCH1;
                 end
+                  
+                DRAW_SPRITE: begin
+                  mem_addr_out <= I + draw_row;
+                  mem_read <= 1;
+                  mem_write <= 0;
+                  state <= FETCH_SPRITE_BYTE;
+                end
+                  
+                FETCH_SPRITE_BYTE: begin
+                  mem_read <= 0;
+                  sprite_byte <= mem_data_in;
+                  for(bit_pos = 0; bit_pos < 8; bit_pos = bit_pos + 1)
+                    begin
+                      if(sprite_byte[7 - bit_pos])
+                        begin
+                          x = (V[opcode[11:8]] + bit_pos) % 64;
+                          y = (V[opcode[7:4]] + draw_row) % 32;
+                          pixel_index = y * 64 + x;
+                          
+                          if(new_display[pixel_index]) 
+                            collision_flag <= 1;
+                          
+                          new_display[pixel_index] <= new_display[pixel_index] ^ 1;
+                        end
+                    end
+                  
+                  if(draw_row == opcode[3:0] - 1)
+                    begin
+                      display <= new_display;
+                      V[15] <= collision_flag ? 1 : 0;
+                      pc <= pc + 2;
+                      state <= FETCH1;
+                    end
+                  else
+                    begin
+                      draw_row <= draw_row + 1;
+                      state <= DRAW_SPRITE;
+                    end
+                end
                 
                 default: begin
                   pc <= pc + 2;
@@ -271,7 +404,3 @@ module chip8_cpu(input wire clk, reset, input wire [7:0] mem_data_in, input wire
         end
     end
 endmodule
-
-
-            
-  	

74331c7ebb14456c22c93e7e9c00b313063cfd23
prxnav2005 <pranavmofficial2005@gmail.com>
2025-06-07 22:55:33 +0530

    feat: add edge-triggered key press detection and previous state tracking

--- src/chip8_keypad.v
+++ src/chip8_keypad.v
@@ -2,30 +2,34 @@
 
 module chip8_keypad(input wire clk, reset, input wire [15:0] keys_raw, output reg key_pressed, output reg [3:0] key_index, output reg [15:0] key_state);
   
+  reg [15:0] prev_key_state;
   integer i;
   
   always @(posedge clk or posedge reset)
     begin
       if(reset)
         begin
-          keys_state <= 16'd0;
-          keys_pressed <= 0;
+          key_state <= 16'd0;
+          key_pressed <= 0;
           key_index <= 4'd0;
+          prev_key_state <= 16'd0;
         end
       else
         begin
-          keys_state <= keys_raw;
+          key_state <= keys_raw;
           key_pressed <= 0;
           key_index <= 4'd0;
           
           for(i = 0; i < 16; i = i + 1)
             begin
-              if(keys_raw[i] && !key_pressed)
+              if(keys_raw[i] && !prev_key_state[i] && !key_pressed)
                 begin
-                  key_index <= i[3:0];
+                  key_index <= i;
                   key_pressed <= 1;
                 end
             end
+          
+          prev_key_state <= keys_raw;
         end
     end
 endmodule

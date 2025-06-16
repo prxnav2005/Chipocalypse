7ce62bbe3e7a10615ece3def49995ac8895246e1
prxnav2005 <pranavmofficial2005@gmail.com>
2025-06-16 22:27:09 +0530

    feat: better rom handling

--- scripts/sim.tcl
+++ scripts/sim.tcl
@@ -1,6 +1,13 @@
 # scripts/sim.tcl
 
+set rom_file [lindex $argv 0]
+if { $rom_file eq "" } {
+    puts "ERROR: No ROM file specified to TCL script."
+    exit 1
+}
+
 open_project /home/prawns/CHIP8_Trial/CHIP8_Trial.xpr
+set_property -name "xelab.more_options" -value "+ROM=$rom_file" -objects [get_filesets sim_1]
 launch_simulation
 run 50000000ns
 exit
